122f258e7b4f51adf08edb30fc300dda85578757
lpd0916 <1985413702@qq.com>
2023-07-10 11:16:27 +0800

    aaa

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import 'regenerator-runtime/runtime';
+if (typeof globalThis.crypto === 'undefined') {
+  globalThis.crypto = require('crypto-browserify');
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
